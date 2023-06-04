5888e2beccb023395547bf5df4a6ba5a254c71ec
Esi McAllen <esimcallen1@gmail.com>
2023-06-04 17:26:35 -0400

    Updating contact page

--- javabeen.js
+++ javabeen.js
@@ -8,3 +8,50 @@ $(document).ready(function() {
     }, 500);
   });
 });
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var subject = document.getElementById('subject').value.trim();
+    var message = document.getElementById('message').value.trim();
+  
+    // Reset error messages
+    document.getElementById('nameError').textContent = '';
+    document.getElementById('emailError').textContent = '';
+    document.getElementById('subjectError').textContent = '';
+    document.getElementById('messageError').textContent = '';
+  
+    // Validate fields
+    var isValid = true;
+    if (name === '') {
+      document.getElementById('nameError').textContent = 'Please enter your name.';
+      isValid = false;
+    }
+  
+    if (email === '') {
+      document.getElementById('emailError').textContent = 'Please enter your email.';
+      isValid = false;
+    }
+  
+    if (subject === '') {
+      document.getElementById('subjectError').textContent = 'Please enter a subject.';
+      isValid = false;
+    }
+  
+    if (message === '') {
+      document.getElementById('messageError').textContent = 'Please enter a message.';
+      isValid = false;
+    }
+  
+    // Submit the form if all fields are valid
+    if (isValid) {
+      // You can add your code here to process the form submission
+      alert('Form submitted successfully!');
+      document.getElementById('contactForm').reset();
+    }
+  });
+  
+  
+  
+  
